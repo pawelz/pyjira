36282457e5ac638e56bf102d95ede1b5d5ada737
Paweł Zuzelski <pzz@touk.pl>
2010-07-29 12:31:59 +0200

    package installer

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# vim:fileencoding=utf-8
+
+# Copyright: © 2010 TouK sp. z o.o. s.k.a.
+# Author:    Paweł Zuzelski <pzz@touk.pl>
+
+from distutils.core import setup
+
+setup(name='pyjira',
+	version='0.0.0',
+	description='JIRA remote access library',
+	author='Paweł Zuzelski',
+	author_email='pzz@touk.pl',
+	url='http://github.com/pawelz/pyjira',
+	packages=['pyjira'],
+)
